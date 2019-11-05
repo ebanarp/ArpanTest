845bb95446cd689bbccd8d314ad1cd3b1ed98e5c
ebanarp <contact.arpanb@gmail.com>
2019-11-05 00:32:36 -0500

    Changed the chrome diver path

--- Test1/test1.py
+++ Test1/test1.py
@@ -2,7 +2,7 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-driver = webdriver.Chrome(executable_path="/home/ebanarp/ArpanTest/chromedriver_linux64/chromedriver")
+driver = webdriver.Chrome(executable_path="/home/ebanarp/ArpanTest/ArpanTest/chromedriver_linux64/chromedriver")
 driver.get("http://newtours.demout.com/")
 print(driver.title)
 driver.close()
